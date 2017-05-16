80d4c82ff8689c7833e8831340256d700d6b6e7d
gilara1 <gilarachmani93@gmail.com>
2017-05-16 18:47:44 +0300

    Menu page with new style!

--- DogCare/Pages/MenuPage.xaml.cs
+++ DogCare/Pages/MenuPage.xaml.cs
@@ -16,23 +16,18 @@ namespace DogCare
         {
             InitializeComponent();
             Title = "Master";
-            BackgroundColor = Color.Gray.WithLuminosity(0.9);
+            BackgroundColor = Color.FromHex("#e6e6e6");
             Icon = Device.OS == TargetPlatform.Android ? "menu.png" : null;
 
             nameOfUser.Text = App.currentOwner.OwnerName;
             var dogName = App.currentDog.DogName;
-            nameOfDog.Text = string.Format("Edit {0}'s profile", dogName);
+            nameOfDog.Text = string.Format(" Edit {0}'s profile", dogName);
 
             //Checking if the user and dog have an image. If not, showing default image.
             if (App.currentOwner.ImageO != null)
                 profileImage.Source = ImageSource.FromStream(() => Utils.ImageStream.ConvertStringToStream(App.currentOwner.ImageO));
             else
                 profileImage.Source = ImageSource.FromFile("User.png");
-
-            if (App.currentDog.ImageD != null)
-                dogImage.Source = ImageSource.FromStream(() => Utils.ImageStream.ConvertStringToStream(App.currentDog.ImageD));
-            else
-                dogImage.Source = ImageSource.FromFile("Dog.png");
         }
 
         async private void MyDogs_Clicked(object sender, EventArgs e)
